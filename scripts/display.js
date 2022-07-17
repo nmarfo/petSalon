37423da63530d6a7e8a7d33b09dea0e85189b22f
nmarfo <106716963+nmarfo@users.noreply.github.com>
2022-07-17 18:38:45 -0400

    Assignment 3 and 4

--- scripts/display.js
+++ scripts/display.js
@@ -1,15 +1,52 @@
 function displayCardsPets(){
+    //travel the array
+
+    //create the card (HTML)
+
+    //inject the card into the HTML (register.html)
+
+
     let card="";
     for(let i=0;i<petSalon.pets.length;i++){
         let pet=petSalon.pets[i];
         card+=`
-        <div class="pet">
+        <div id="${pet.id}" class="pet">
             <h4>Name: ${pet.name}</h4>
             <p>Age: ${pet.age}</p>
             <p>Gender: ${pet.gender}</p>
+            <p>Breed: ${pet.breed}</p>
+            <p>Owner: ${pet.owner}</p>
+            <p>Service:${pet.service}</p>
+            <p>Phone: ${pet.phone}</p>
+            
         </div>
 
         `;
+
+        console.log(card);
     }
     document.getElementById("pet-cards").innerHTML=card;
+}
+
+function diplayPetsTable(){
+   let tr="";
+   for(let i=0;i<petSalon.pets.length;i++){
+    let pet=petSalon.pets[i];
+    tr+=`
+    <tr id="${pet.id}">
+        <td> ${pet.name}</td>
+        <td> ${pet.age}</td>
+        <td> ${pet.gender}</td>
+        <td> ${pet.breed}</td>
+        <td> ${pet.owner}</td>
+        <td> ${pet.service}</td>
+        <td> ${pet.phone}</td>
+        <td><button class="btn btn-danger" onclick="deletePet(${pet.id})"> Delete </button></td>
+        
+    </tr>
+    
+    `;
+    document.getElementById("pet-table").innerHTML=tr;
+}
+console.log(tr);
 }
--- scripts/register.js
+++ scripts/register.js
@@ -1,72 +1,144 @@
-//arrays[]
-//objects{}
-let petSalon = {
-    //attributes
-    name:"The Fashion Pet",
-    address:"486 Mosholu Parkway",
-    hours:{
-        open:"9:30am",
-        close:"6:00pm"
-    },
-     //anonymous object
-    pets:[]
-}
-//constructor function
-//---------------Parameters/ local variables ----------->
-function Pet(name,age,gender,breed,service,ownerName,contactPhone){
-    //attributes = parameters
-    this.name=name;
-    this.age=age;
-    this.gender=gender;
-    this.breed=breed;
-    this.service=service;
-    this.owner=ownerName;
-    this.phone=contactPhone;
-}
-//creating the function
-function displayInfo(){
-    document.getElementById("Info").innerHTML=`Welcome to ${petSalon.name} family. It is available at ${petSalon.address} from ${petSalon.hours.open} to ${petSalon.hours.close}`;
-}
+    //arrays[]
+    //objects{}
+    let petSalon = {
+        //attributes
+        name:"The Fashion Pet",
+        address:"486 Mosholu Parkway",
+        hours:{
+            open:"9:30am",
+            close:"6:00pm"
+        },
+        //anonymous object
+        pets:[]
+    }
+    //counter
+    let c=0;
+    //constructor function
+    //---------------Parameters/ local variables ----------->
+    function Pet(name,age,gender,breed,service,ownerName,contactPhone){
+        //attributes = parameters
+        this.name=name;
+        this.age=age;
+        this.gender=gender;
+        this.breed=breed;
+        this.service=service;
+        this.owner=ownerName;
+        this.phone=contactPhone;
+        this.id=c++;
+    }
+    //creating the function
+    function displayInfo(){
+        document.getElementById("Info").innerHTML=`Welcome to ${petSalon.name} family. It is available at ${petSalon.address} from ${petSalon.hours.open} to ${petSalon.hours.close}`;
+    }
 
-function displayNumberOfPets(){
-    document.getElementById("number-pets").innerHTML=`There are ${petSalon.pets.length} pets`;
-}
-function register(){
-    //get info from the input
-    let petName=document.getElementById("addPetName").value;
-    let petAge=document.getElementById("addPetAge").value;
-    let petGender=document.getElementById("addPetGender").value;
-    let petBreed=document.getElementById("addPetBreed").value;
-    let petOwner=document.getElementById("addPetOwnerName").value;
-    let petPhone=document.getElementById("addPetPhone").value;
-
+    function displayNumberOfPets(){
+        document.getElementById("number-pets").innerHTML=`There are ${petSalon.pets.length} pets`;
+    }
     
-    //create the obj
-    let newPet=new Pet(petName,petAge,petGender,petBreed,petOwner,petPhone);
-    console.log(newPet);
-    //create push
-    petSalon.pets.push(Pet);
-    console.log(petSalon.pets);
-    displayNumberOfPets();
-    displayCardsPets();
-}
-function init(){
-//creating objectss
-let scooby = new Pet("Scooby",50,"Male","Dane","Grooming","Shaggy","666-666-666");
-let bingo = new Pet("Bingo",40,"Male","Mixed","Vaccination","Nolan","555-666-666");
-let nyame = new Pet("Nyame",80,"Female","German Shepherd","cutting nails","Maverick","777-777-777");
-petSalon.pets.push(scooby,bingo,nyame);
-console.log(petSalon.pets);
-//calling the function
-displayInfo();
-displayNumberOfPets();
-displayCardsPets();
 
 
-//hook events
+    function isValid(aPet){
+        let valid=true;
+        if(aPet.name==""){// is empty?
+            valid=false;
+            //alert("Please enter a pet name to be registered");
 
-}
+        } 
+
+        return valid;
+    }
 
 
-window.onload=init;
+    function register(){
+        //get info from the input
+        let petName=document.getElementById("addPetName").value;
+        let petAge=document.getElementById("addPetAge").value;
+        let petGender=document.getElementById("addPetGender").value;
+        let petBreed=document.getElementById("addPetBreed").value;
+        let petService=document.getElementById("selService").value;
+        let petOwner=document.getElementById("addPetOwnerName").value;
+        let petPhone=document.getElementById("addPetPhone").value;
+        
+       
+
+        
+        //create the obj
+        let newPet=new Pet(petName,petAge,petGender,petBreed,petService,petOwner,petPhone);
+        if(isValid(newPet)){
+            petSalon.pets.push(newPet);
+            displayNumberOfPets();
+            console.log(petSalon.pets);
+            //displayCardsPets();
+            diplayPetsTable();
+            clearInputs();
+        }else{
+            alert("Please enter the pet name to Register");
+        }
+
+        
+    }
+    function clearInputs(){
+        document.getElementById("addPetName").value="";
+        document.getElementById("addPetAge").value="";
+        document.getElementById("addPetGender").value="";
+        document.getElementById("addPetBreed").value="";
+        document.getElementById("addPetOwnerName").value="";
+        document.getElementById("addPetPhone").value="";
+
+    }
+     function deletePet(petID){
+        document.getElementById(petID).remove();
+        //travel the array
+        let petIndex;
+        for(let i=0;i<petSalon.pets.length;i++){
+            let pet=petSalon.pets[i];
+        //compare the id of the pet
+        if(pet.id == petID){
+            petIndex=i;// get the pet index
+            }
+        }
+        petSalon.pets.splice(petIndex,1);//remove from the array
+        displayNumberOfPets();//update the displayNumberOfPets()
+        diplayPetsTable();
+     }
+     function search(){
+        let searchItem=document.getElementById("addSearch").value;
+        console.log("Searching for pets ..." +searchItem)
+        for(let i=0;i<petSalon.pets.length;i++){
+            let pet=petSalon.pets[i];
+            if(searchItem.toLowerCase() == pet.name.toLowerCase()){
+                document.getElementById(pet.id).classList.add("selected");
+            }else{
+                document.getElementById(pet.id).classList.remove("selected");
+            }
+
+        }
+    }
+
+        
+        
+     
+
+    function init(){
+        //creating objectss
+        let scooby = new Pet("Scooby",50,"Male","Dane","Grooming","Shaggy","666-666-666");
+        let bingo = new Pet("Bingo",40,"Male","Mixed","Vaccination","Nolan","555-666-666");
+        let nyame = new Pet("Nyame",80,"Female","German Shepherd","cutting nails","Maverick","777-777-777");
+        petSalon.pets.push(scooby,bingo,nyame);
+        console.log(petSalon.pets);
+        //calling the function
+        displayInfo();
+        //register();
+        displayNumberOfPets();
+        //displayCardsPets();
+        diplayPetsTable();
+
+
+
+    //hook events
+
+    }
+
+
+    window.onload=init;
 
